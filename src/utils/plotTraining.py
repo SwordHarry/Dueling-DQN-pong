711f708072f3cfa50fdbb0ccb088bc86df77f5d7
swordharry <2398960068@qq.com>
2020-12-21 00:17:13 +0800

    add: img save

--- src/utils/plotTraining.py
+++ src/utils/plotTraining.py
@@ -7,20 +7,15 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 from matplotlib.pyplot import MultipleLocator
 
-from src.config.config import TRAIN_LOG_PATH, TRAIN_LOG_FILE, IMG_PATH
+from src.config.config import TRAIN_LOG_PATH, TRAIN_LOG_FILE, IMG_PATH, TRAIN_LOG_FILE_LR
 
 
-def moving_average(a, n=3):
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
-
-
-def plot_training(episodes, rewards, is_save=False):
+# show or save tht plt of the train
+def plot_training(episodes_list, rewards_list, is_save=True, is_compare=True):
     clear_output(True)
     # title and label
     plt.title('reward learning curve', fontsize=24)
-    plt.xlabel("episode")
+    plt.xlabel("episode(frames/k)")
     plt.ylabel("mean reward")
 
     # 坐标刻度设置
@@ -30,8 +25,13 @@ def plot_training(episodes, rewards, is_save=False):
     ax.xaxis.set_major_locator(x_major_locator)
     ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
     ax.yaxis.set_major_locator(y_major_locator)
+
+    # fig, ax = plt.subplots()
     # set the plot
-    plt.plot(episodes, rewards)
+    plt.plot(episodes_list[0], rewards_list[0], color='blue', label="origin")
+    if is_compare:
+        plt.plot(episodes_list[1], rewards_list[1], color="red", label="lr")
+    plt.legend()
     if is_save:
         plt.savefig(IMG_PATH)
     plt.show()
@@ -39,16 +39,28 @@ def plot_training(episodes, rewards, is_save=False):
 
 # output to plt
 def save_plt_reward_frame():
+    episodes_list = []
+    rewards_list = []
+    e, r = get_episodes_rewards(TRAIN_LOG_FILE)
+    episodes_list.append(e)
+    rewards_list.append(r)
+    e_lr, r_lr = get_episodes_rewards(TRAIN_LOG_FILE_LR)
+    episodes_list.append(e_lr)
+    rewards_list.append(r_lr)
+    plot_training(episodes_list, rewards_list)
+
+
+def get_episodes_rewards(file_str):
     episode_list = []
     reward_list = []
-    with open(os.path.join(TRAIN_LOG_PATH, TRAIN_LOG_FILE)) as csv_file:
+    with open(os.path.join(TRAIN_LOG_PATH, file_str)) as csv_file:
         csv_reader = csv.reader(csv_file)  # 使用csv.reader读取csvfile中的文件
         title_list = next(csv_reader)  # 读取第一行每一列的标题，或者略过第一行有问题的数据
         for row in csv_reader:  # 将csv 文件中的数据保存到birth_data中
-            episode = row[1]
-            if len(episode_list) > 0 and episode_list[-1] == episode:
-                continue
+            episode = row[0]
+            episode = int(episode)/1000
             reward = row[2]
             episode_list.append(episode)
             reward_list.append(reward)
-    plot_training(episode_list, reward_list)
+    return list(map(int, episode_list)), list(map(float, reward_list))
+
